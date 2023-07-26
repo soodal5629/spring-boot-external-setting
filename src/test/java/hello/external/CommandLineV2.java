dc3861198dfa085e59c8aa37675f600a3cca5843
jcl <chaelynjang@lotte.net>
2023-07-26 16:38:15 +0900

    커맨드 라인 옵션 인수(String args[] -> key-value 파싱) 테스트

---
+++ src/test/java/hello/external/CommandLineV2.java
@@ -0,0 +1,29 @@
+package hello.external;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
+
+import java.util.List;
+import java.util.Set;
+
+@Slf4j
+public class CommandLineV2 {
+    public static void main(String[] args) {
+        for (String arg : args) {
+            log.info("arg = {}", arg);
+        }
+        // key-value 형식으로 파싱해줌
+        ApplicationArguments appArgs = new DefaultApplicationArguments(args);
+        log.info("SourceArgs = {}", List.of(appArgs.getSourceArgs()));
+        log.info("NonOptionArgs = {}", appArgs.getNonOptionArgs());
+        log.info("getOptionNames = {}", appArgs.getOptionNames());
+
+        Set<String> optionNames = appArgs.getOptionNames();
+        for (String optionName : optionNames) {
+            log.info("option arg {} = {}", optionName, appArgs.getOptionValues(optionName));
+        }
+
+
+    }
+}
